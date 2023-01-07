ce3ce0be1cd1e4b117e316984ca0018cd1f4ae85
kr-2003 <kumar.kr.abhinav@gmail.com>
2023-01-07 16:26:12 +0530

    ManagerPage:(Login, Dashboard(Items))

---
+++ app/components/Riders.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Riders() {
+  return (
+    <div className="riders-wrapper w-[95%] h-[80%] p-5 m-6 rounded-xl">
+      <div className="relative">
+        <h1 className="absolute left-0 text-[40px] flex flex-end font-bold">
+            Riders
+        </h1>
+      </div>
+      <div className="h-[70%] overflow-scroll mt-20">
+      </div>
+      <div className="w-[100%] flex justify-center mt-4">
+        <button className="z-auto bg-[#111111] text-[#02f97b] px-5 py-5 rounded-3xl w-[50%] font-bold">
+          Allot Items to Riders
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Riders;
--- app/pages/manager/index.tsx
+++ app/pages/manager/index.tsx
@@ -1,10 +1,46 @@
 import * as React from 'react';
-
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from "next/router";
 export default function ManagerHome() {
-
+    const router = useRouter();
+    const onLogin = () => {
+        //verification
+        router.push("/manager/dashboard")
+    }
     return (
         <>
-            <h1>Manage Home Page</h1>
+            <div className='absolute'>
+                <Image width={200} height={100} alt="Grow Simplee Logo" src="/interIITLogo.png"></Image>
+            </div>
+            <div className='flex justify-center items-center h-screen bg-[#111111]'>
+
+                <div className='w-[400px] h-auto'>
+                    <div className=''>
+                        <Image width={500} height={100} alt="Grow Simplee Logo" src="/growSimplee.png"></Image>
+                    </div>
+                    <h1 className='text-center text-2xl font-bold text-[#02f97b] mb-10'>LOGIN AS MANAGER</h1>
+                    <div className='bg-[#cee0d5] w-[100%] h-[250px] rounded-lg p-5'>
+                        <div className='mb-5'>
+                            <p className='text-[#696b6a]'>Username</p>
+                            <input className='bg-[#e4f0e8] border-b-2 border-[#84a38f] rounded h-10 p-3 w-[100%]'></input>
+                        </div>
+                        <div className='mb-6'>
+                            <p className='text-[#696b6a]'>Password</p>
+                            <input className='bg-[#e4f0e8] border-b-2 border-[#84a38f] rounded h-10 p-3 w-[100%]' type={"password"}></input>
+                        </div>
+                        <div className='flex justify-center'>
+                            <div onClick={onLogin} className="w-[30%]">
+                                <button className='z-auto bg-[#111111] text-[#02f97b] px-5 py-2 rounded-md w-[100%]' onClick={() => { }}>
+                                    Login
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
