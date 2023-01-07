4ec98215b7016e248c51d08541a45540cc9b607b
Mukul Jain <mukul.jain1906@yahoo.com>
2023-01-07 19:05:34 +0530

    Backend adjustments

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# Route-Planning
-
+# Route-Planning for Optimised on-time Delivery
+This app is used to achieve Efficient On-Time Delivery and serve as a platform for warehouse managers and delivery personnels.
 ## Installation
 
 ### Requirements
 1. Python 3.7+ 
 
 ### Steps to Reproduce
-1. `cd ./server`
+1. `cd ./server/`
 1. `pip install -r requirements.txt`
 1. `uvicorn main:app`
 
--- server/main.py
+++ server/main.py
@@ -1,38 +1,27 @@
-from typing import List
-
-import schemas
 import services
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
-
-app = FastAPI()
-
+from fastapi import FastAPI
+from settings import settings
+from router import UserRouter
 
 services.create_database()
 
 
+tags_metadata = [
+    {"name": "Users", "description": "User Authentication"},
+]
+
+app = FastAPI(
+    title=settings.TITLE,
+    version=settings.VERSION,
+    description=settings.DESCRIPTION,
+    license_info={
+        "name": settings.LICENSE_NAME,
+    },
+    openapi_tags=tags_metadata,
+)
+
+app.include_router(UserRouter)
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(services.get_db)):
-    db_user = services.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return services.create_user(db=db, user=user)
-
-
-@app.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(services.get_db)):
-    users = services.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(services.get_db)):
-    db_user = services.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
