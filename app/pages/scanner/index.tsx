4eabbc05c7658ed24527cb38e3124ab624b9f6cc
Mihir Karandikar <mihirkkarandikar@gmail.com>
2023-01-07 18:14:05 +0530

    basic scanner page layout

--- app/pages/scanner/index.tsx
+++ app/pages/scanner/index.tsx
@@ -1,21 +1,41 @@
 import * as React from 'react';
+import Image from 'next/image';
+import groceryImg from '../../public/grocery.png'
 
 export default function Scanner() {
 
     return (
         <>
             
-            <nav className='flex justify-between items-center bg-green-300'>
+            <nav className='flex justify-between items-center bg-green-300 p-1 w-[100%]'>
 
                 <span>Grow Simplee</span>
                 
-                <ul className='flex justify-evenly'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                <ul className='flex justify-around'>
+                    <li className='p-2'>Home</li>
+                    <li className='p-2'>About</li>
+                    <li className='p-2'>Contact</li>
                 </ul>
 
             </nav>
+
+            <div className='absolute flex items-center space-x-3 left-4 top-28'>
+                <Image
+                    src={groceryImg}
+                    alt="Picture of the author"
+                    className='max-w-sm'
+                />
+                <div className='bg-gray-500 border-2 rounded-md p-2'>   
+                    <ul>
+                        <li>Name : zxc</li>
+                        <li>Volume : 10</li>
+                        <li>Weight : 23</li>
+                        <li>id : as123as</li>
+                    </ul>
+                </div>
+                
+            </div>
+
 
         </>
     )
