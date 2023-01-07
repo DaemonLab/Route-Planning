ad8d01a73a88ccd4e710c26ef539d906e7f0b9ea
Mukul Jain <mukul.jain1906@yahoo.com>
2023-01-07 21:06:03 +0530

    checking automated deployment

--- server/settings.py
+++ server/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    TITLE: str = "Route Planning for Optimised on-time Delivery"
+    TITLE: str = "Route Planning for Optimised on time Delivery"
     VERSION: str = "1.0.0"
     DESCRIPTION: str = "This app is used to achieve Efficient On-Time Delivery and serve as a platform for warehouse managers and delivery personnels."
     LICENSE_NAME: str = "GNU Gneral Public License 3.0"
