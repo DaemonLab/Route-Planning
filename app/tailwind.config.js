6d33e2103fbdcc4134ec8ef498602e5544cb65fb
Mukul Jain <mukul.jain1906@yahoo.com>
2023-01-09 00:31:52 +0530

    Tailwind changes

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,6 +2,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
