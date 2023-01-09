a2b2934b6d64d1017bfbd24fa8914355387b79c8
Mukul Jain <mukul.jain1906@yahoo.com>
2023-01-09 10:09:11 +0530

    Added Login

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { Main } from "next/document";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Feature from "../components/Feature";
