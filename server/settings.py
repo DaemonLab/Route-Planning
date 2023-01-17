b6052e292f0d08a447abcfb337bf52484ce05419
Mukul Jain <mukul.jain1906@yahoo.com>
2023-01-17 22:28:15 +0530

    Spelling Correction

--- server/settings.py
+++ server/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     TITLE: str = "Route Planning for Optimised on-time Delivery"
     VERSION: str = "1.0.0"
     DESCRIPTION: str = "This app is used to achieve Efficient On-Time Delivery and serve as a platform for warehouse managers and delivery personnels."
-    LICENSE_NAME: str = "GNU Gneral Public License 3.0"
+    LICENSE_NAME: str = "GNU General Public License 3.0"
     ENVIRONMENT: str = "DEV"
     MONGO_URL: str
 
