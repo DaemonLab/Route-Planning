93f6c25c06d789896211d28623f85721b5495a68
Mihir Karandikar <mihirkkarandikar@gmail.com>
2023-01-26 18:53:50 +0530

    add cpp algorithm

--- server/services/__init__.py
+++ server/services/__init__.py
@@ -2,3 +2,4 @@ from .user import *
 from .rider import *
 from .item import *
 from .navigation import *
+from .map import *
--- server/services/navigation.py
+++ server/services/navigation.py
@@ -1,24 +1,78 @@
 import serializers
+import services
 from database import item_db, rider_db
 from fastapi import HTTPException
 from models import Item, Rider
 from typing import List
+import subprocess
+import datetime
 
 def rider_update():
     print("Updating Rider Location")
 
-# https://stackoverflow.com/questions/65408371/how-to-use-python-to-input-to-and-get-output-from-c-program
-
-
 def dispatch():
-    try:
 
-        items = item_db.find()
-        riders = rider_db.find()
-        # Run algorithm and assign deliveries to riders
+    items  = serializers.items_serializer(item_db.find())
+    riders = serializers.riders_serializer(rider_db.find())
 
-    except:
-        return 0
+    num_items  = len(items)
+    num_riders = len(riders)
+
+    dist = services.get_distance_matrix(items,num_items)
+
+    f = open("./algorithm/input.in", "w")
+
+    print(num_items,file=f)
+
+    for i in range(num_items+1):
+        for j in range(num_items+1):
+            if i==j :
+                continue
+            print(dist[i][j],file=f)
+
+    for item in items:
+        print(int(item['volume']),file=f)
+
+    for item in items:
+        print(int(item['weight']),file=f)
+        # print(str(item['edd']),file=f)
+
+    print(num_riders,file=f)
+
+    for rider in riders:
+        print(int(rider['bag_volume']),file=f)
+
+    f.close()
+
+    subprocess.call("algorithm\dispatch.exe",shell=True) 
+
+    f = open("./algorithm/output.out", "r")
+
+    deliveries = dict()
+
+    for i in range(num_riders):
+
+        order = []
+
+        while(True):
+            
+            result = int(f.readline())
+
+            if result==-1:
+                break
+
+            #Items order obtained from algorithm is 1-indexed
+            #We convert it to zero-indexed
+            
+            order.append(result-1)
+
+        deliveries[i] = order
+
+    print(deliveries)
+
+
+
+    return {"done" : str(deliveries)}
 
 def assign_pickup_items(free_riders,pickup_items):
   #Run algorithm and assign pickups to riders
