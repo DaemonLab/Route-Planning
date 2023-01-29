624d811a5ee0ea58735d3e444e69256580803198
Mihir Karandikar <mihirkkarandikar@gmail.com>
2023-01-29 21:38:44 +0530

    update schema

--- app/@types/rider.d.ts
+++ app/@types/rider.d.ts
@@ -1,15 +1,18 @@
 import { Location , RouteLocation ,RouteDetail } from "./route";
 
 export interface Rider {
+  
   rider_id: string;
   name: string;
   age: number;
   bag_volume: number;
+
   current_location: RouteLocation;
   current_route: RouteLocation[];
   route_details: RouteDetail[]; 
   route_polyline: RouteLocation[]
   route_index: number; 
+  
   tasks: Task[];
   task_index: number;
 };
--- server/models/rider.py
+++ server/models/rider.py
@@ -12,11 +12,13 @@ class Rider(BaseModel):
     name: str = Field(..., title="Name")
     age: int = Field(..., title="Age")
     bag_volume: int = Field(..., title="Bag Volume")
+
     current_location: RouteLocation = None
     current_route: List[RouteLocation] = []
     route_details: List[RouteDetail] = []
     route_polyline: List[RouteLocation] = []
     route_index: int = Field(..., title="Route Index")
+    
     tasks: List[Task] = []
     task_index: int = Field(..., title="Task Index")
 
